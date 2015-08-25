306d4b89d77d9e577b9d99f18d55d9419699b9db
John Beech <john.beech@bbc.co.uk>
2015-08-25 09:42:27 +0100

    Added access cnontrol headers for all sources.

--- index.js
+++ index.js
@@ -7,6 +7,12 @@ var isSupportedGenerator = require('./lib/isSupportedGenerator/command')
 
 var events = [];
 
+app.get('/*', function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    next();
+});
+
 app.get('/deviceIdentification', function(req, res) {
     var requestEvent = {
         userAgent: req.get('user-agent'),
